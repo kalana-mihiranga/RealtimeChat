495429b3591f28a5e488e11294873897b30e027b
Kalana Mihiranga <kalanamihiranga97@gmail.com>
2023-12-26 22:04:52 +0530

    finalize the chat vapp

---
+++ README.md
@@ -0,0 +1,17 @@
+# <b>Chat App</b>
+
+<p align="center">
+    <img src="src/Screenshot from 2023-12-26 21-50-59.png" width="500px" height="250px">
+</p>
+<p align="center">
+    <img src="src/Screenshot from 2023-12-26 21-51-27.png" width="500px" height="250px">
+</p>
+
+<p>
+<br><br>
+Realtime chat app with Node JS, React JS and ChatEngine.io.
+
+This feature rich application will have: User authentication, socket connections, real-time messaging, image and file attachments, group chats and DMs, read receipts
+</p>
+
+
--- front-end/src/ChatsPage.jsx
+++ front-end/src/ChatsPage.jsx
@@ -1,18 +1,16 @@
 
-import { PrettyChatWindow } from 'react-chat-engine-pretty';
+import { PrettyChatWindow } from "react-chat-engine-pretty";
 
+const ChatsPage = (props) => {
+  return (
+    <div className="background">
+      <PrettyChatWindow
+       projectId="b75e5bd5-cd84-404c-b820-06feff8c98c0"
+        username={props.user.username}
+        secret={props.user.secret}
+      />
+    </div>
+  );
+};
 
-const ChatsPage=(props)=>{
-    
-    return     (    <div style={{height:'100vh'}}>
-    <PrettyChatWindow
-projectId="b75e5bd5-cd84-404c-b820-06feff8c98c0"
-username={props.user.username}
-secret={props.user.secret}
-style={{ height: '100vh' }}
-/>
-</div>)
-
-
-}
-export default ChatsPage
+export default ChatsPage;
