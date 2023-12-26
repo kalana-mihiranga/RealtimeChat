8a69bf77f00bdab0991e835c84727167ddfaeb5b
Kalana Mihiranga <kalanamihiranga97@gmail.com>
2023-12-26 15:14:16 +0530

    connect nodejs to chatengine

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,29 @@
 const express = require("express");
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
+  
   const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+
+  try{
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "Private-Key": "fc3ead56-f3fc-474c-b3f8-8d394226ca6d" } }
+    );
+    return res.status(r.status).json(r.data)
+
+  }
+  catch(e){
+    return res.status(e.response.status).json(e.response.data)
+
+  }
+
 });
 
 app.listen(3001);
